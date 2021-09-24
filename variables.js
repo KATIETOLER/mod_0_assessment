8afa794d89df40761fd1ca3e0e3816344aaf065c
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-09-24 11:19:45 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+var movie = "The Lord of the Rings"
+var isEntertaining = true
+movie = "All Good Things"
+
+var num4 = 9
+var num6 = 27
+
+var product = (num4 + num6)
+
+var favoriteFoods = ["Chocolate Peanutbutter Cups", "Cheese", "Breakfast Burrito"]
+
+
+favoriteFoods.shift()
+console.log(movie.length)
