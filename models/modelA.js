71475b0983c78c1170369195d85283a21abf5d8a
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-09-24 11:39:33 -0600

    Add modelsdir

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+
+
+var dinosaur = {
+  spikeNums: 15,
+  teethNums: 100,
+  favoriteFoods: ["People", "Other Dinos", "Animals"],
+  bestFriends: ["Andrew", "Leslie", "Jessica"],
+  dinoName: "Tiffany",
+  dinoType: "Stegosaurus",
+
+}
---
+++ models/modelB.js
@@ -0,0 +1,14 @@
+
+var developer = {
+  tallInFt: 5.2,
+  coffeeCups: 3,
+  clothes: "relaxed",
+  shoes: "barefoot",
+  hasHeadphones: true,
+  hasBreakfast: false,
+  notebooks: ["Calendar","Mod0 Notes", "Bullet Journal"],
+  plantsInOffice: ["fern", "Snake plant", "Bird of Paradise"]
+}
+
+var devLen = ((developer) + ("30 yrs."))
+console.log(devLen)
